184e4c827c528410982f95f0434c2cc18104aa76
RabaS <sebastian-stefan.raba@haufe.com>
2022-09-18 21:51:05 +0300

    feat(certificates-list): add dummy teable component for cetificates list

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import CertificatesTable from "./components/certificates/CertificatesTable";
 import Navbar from "./components/navbar/Navbar";
 
 function App() {
@@ -8,7 +9,7 @@ function App() {
       <Navbar />
       <Routes>
         <Route path="/" element={<div>Home</div>} />
-        <Route path="/certificates" element={<div>Certificates</div>} />
+        <Route path="/certificates" element={<CertificatesTable/>} />
         <Route path="/register" element={<div>Register Certificates</div>} />
       </Routes>
     </Router>
